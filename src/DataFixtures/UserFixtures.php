25185f3df07510f5add479965719ed3039750ad5
sds912 <senghor.pape912@hotmail.com>
2019-12-25 18:32:14 +0000

    create and register a super admin

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $superAdmin = new User();
+
+        $superAdmin->setUsername('supperAdmin');
+        $superAdmin->setPassword($this->encoder->encodePassword($superAdmin,'super'));
+        $superAdmin->setRoles(['ROLE_SUPER_ADMIN, ROLE_ADMIN']);
+        $manager->persist($superAdmin);
+        $manager->flush();
+    }
+}
