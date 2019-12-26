773b73af5abef9e59b927de76ce986a5248078e2
sds912 <senghor.pape912@hotmail.com>
2019-12-26 16:01:13 +0000

    authentication with jwt token

--- src/Controller/SuperAdminController.php
+++ src/Controller/SuperAdminController.php
@@ -3,11 +3,35 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class SuperAdminController extends Controller{
+
+
+    /** @var UsersRepository $userRepository */
+    private $usersRepository;
+
+
+     /**
+     * AuthController Constructor
+     *
+     * @param UsersRepository $usersRepository
+     */
+    
+    public function __construct(UserRepository $usersRepository)
+    {
+        $this->usersRepository = $usersRepository;
+    }
+
+
+   public function addNewUserSystem(){
+
+   
+
+    }
 
    
 }
